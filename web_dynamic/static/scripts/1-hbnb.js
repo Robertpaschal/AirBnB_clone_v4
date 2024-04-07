8bbdfbd0fda647b2a197cee9c8e5e87ebf3e0dc6
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-04-07 20:26:41 +0100

    retiretrieving checked amenities from DOM

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    var selectedAmenities = [];
+
+    $('input[type="checkbox"]').change(function () {
+        const amenityId = $(this).data('id');
+        const amenityName = $(this).data('name');
+
+        if ($(this).prop('checked')) {
+            selectedAmenities.push({id: amenityId, name: amenityNam});
+        } else {
+            selectedAmenities = selectedAmenities.filter(function (amenity){
+                return amenity.id !== amenityId;
+            });
+        }
+
+        const amenitiesList = selectedAmenities.map(function(amenity) {
+            return amenity.name;
+        }).join(', ');
+
+        $('#amenities h4').text('Selected Amenities: ' + amenitiesList);
+    });
+});
